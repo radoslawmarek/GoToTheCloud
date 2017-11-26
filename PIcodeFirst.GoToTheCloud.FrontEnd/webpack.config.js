4ddf14c0e2f90dbfc52f381aa51343e06d909005
Radosław Marek <radoslaw.marek@codefirst.pl>
2017-11-26 19:06:46 +0100

    App decoration: - added Bootstrap - added and configured webpack - added navigation bar

---
+++ PIcodeFirst.GoToTheCloud.FrontEnd/webpack.config.js
@@ -0,0 +1,33 @@
+﻿var webpack = require('webpack');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        path: __dirname + '/wwwroot',
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            { test: /\.(png|jpg|gif|svg)$/, loader: 'file-loader', options: { name: '[name].[ext]?[hash]' } },
+            { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+            { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+            { test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/, loader: "url-loader" },
+            { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            // In case you imported plugins individually, you must also require them here:
+            Util: "exports-loader?Util!bootstrap/js/dist/util",
+            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+        })
+    ]
+};
